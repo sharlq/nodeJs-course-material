de21c8926974bf8fd573d2eeb6bd6243912b2399
sharlq <shehabakhader@gmail.com>
2023-05-28 11:05:54 +0300

    hmm

--- database/sequelize-intro/models/user.js
+++ database/sequelize-intro/models/user.js
@@ -1,27 +1,33 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../utils/database');
 
-const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-    validate: {
-      isEmail: true,
+const User = sequelize.define(
+  'User',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
     },
   },
-});
+  {
+    tableName: 'users',
+  }
+);
 module.exports = User;
